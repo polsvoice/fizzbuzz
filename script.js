8e8b0618045add9b3804745954a40b907c66d215
Brent Bartlett <brent.bartlett1@gmail.com>
2018-04-30 12:42:26 -0700

    redone with better tests

--- script.js
+++ script.js
@@ -1,34 +1,22 @@
+
 var fizzBuzz = function(){
     return {
-        printNums: function( startNum, lastNum ){
-            var resultArr = []
-                , ctr
-                , result;
-            
-            ctr = startNum || 0;
-            
-            while ( ctr <= lastNum ){
-                result = this.testNum( ctr );
-                resultArr.push( result );
-                ctr++;
-            }
-            return resultArr;
-        },
-        testNum: function( num ){
-            if ( num === 0 ){
-                return num;
-            } else if ( !( num % 3 ) && !( num % 5 ) ){
+        testNum: function(num){
+            if (!(num % 3) && !(num % 5)){
                 return "FizzBuzz";
-            } else if ( !( num % 3 ) ){
+            }
+            else if (!(num % 3)){
                 return "Fizz";
-            } else if ( !( num % 5 ) ){
+            } else if (!(num % 5)){
                 return "Buzz";
             } else {
                 return num;
             }
         },
         init: function(){
-            console.log( this.printNums( 0, 100 ) );
+            for (i = 1; i <= 100; i++){
+                console.log(this.testNum(i));
+            }
         }
     }
 }();
